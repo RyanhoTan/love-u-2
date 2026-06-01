16811a980a5a3c251b3c2a0e4b7b8b4b0b4571fa
tyh <2939056298@qq.com>
2026-06-01 14:54:42 +0800

    ﻿chore(eslint): 添加 eslint-plugin-react-native 并配置规则
    在 package.json 中添加了 eslint 和 eslint-plugin-react-native 依赖，并在 app/eslint.config.js 中配置了 react-native/no-unused-styles 警告规则。

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const reactNative = require("eslint-plugin-react-native");
 
-module.exports = defineConfig([expoConfig]);
+module.exports = defineConfig([
+  ...expoConfig,
+  {
+    plugins: {
+      "react-native": reactNative,
+    },
+    rules: {
+      "react-native/no-unused-styles": "warn",
+    },
+  },
+]);
